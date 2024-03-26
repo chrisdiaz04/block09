6dfa3279e9779bc412e63f7766528dd07587a194
chrisdiaz04 <diazchris27586@gmail.com>
2024-03-26 17:37:25 -0500

    updated code

--- Personal_Site/success.js
+++ Personal_Site/success.js
@@ -1,6 +1,4 @@
-alert("YOU WILL BE REDIRECTED BACK TOT THE CONTACT PAGE!")
-
 setTimeout(() => {
     window.location.href = window.location.origin + '/block09/Personal_Site/Contact.html'
-  }, 5000)
+  },10000)
 
