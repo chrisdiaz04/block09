fee6241e9d6e10ea4331a5c8042502e2b3327d8e
chrisdiaz04 <diazchris27586@gmail.com>
2024-03-26 17:31:39 -0500

    Minor details and added JS

---
+++ Personal_Site/success.js
@@ -0,0 +1,6 @@
+alert("YOU WILL BE REDIRECTED BACK TOT THE CONTACT PAGE!")
+
+setTimeout(() => {
+    window.location.href = window.location.origin + '/block09/Personal_Site/Contact.html'
+  }, 5000)
+
